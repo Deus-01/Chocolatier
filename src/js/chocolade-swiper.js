63bb6e3b66bbe45d4fcb5504a2a15468db985f8a
Иван-Т0 <vanyataran24@gmail.com>
2023-03-25 12:37:14 +0200

    update

--- src/js/chocolade-swiper.js
+++ src/js/chocolade-swiper.js
@@ -7,34 +7,30 @@ const swiper = new Swiper(".chocolate-slider", {
   pagination: {
     el: ".chocolate-swiper-pagination",
     clickable: true,
-    dynamicBullets: true,
+    dynamicBullets: false,
   },
 
   
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-      stopOnLastSlide: false,
+    // autoplay: {
+    //   delay: 5000,
+    //   disableOnInteraction: false,
+    //   stopOnLastSlide: false,
 
-    },
+    // },
   speed: 1500,
-  //   breakpoints: {
-  //     // when window width is >= 320px
-  //     320: {
-  //       slidesPerView: 2,
-  //       spaceBetween: 20
-  //     },
-  //     // when window width is >= 480px
-  //     480: {
-  //       slidesPerView: 3,
-  //       spaceBetween: 30
-  //     },
-  //     // when window width is >= 640px
-  //     640: {
-  //       slidesPerView: 4,
-  //       spaceBetween: 40
-  //     }
-  //   }
+    breakpoints: {
+      // when window width is >= 320px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+     
+    }
 });
