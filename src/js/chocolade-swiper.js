6000243e0f61e7ce35ac8cb98c8ca1dd6ccd470b
Иван-Т0 <vanyataran24@gmail.com>
2023-03-26 18:23:00 +0300

    chocolate.css
    переделал js поменял на другой слайдер, и сделал css оформление

--- src/js/chocolade-swiper.js
+++ src/js/chocolade-swiper.js
@@ -1,36 +1,71 @@
-const swiper = new Swiper(".chocolate-slider", {
-  // Optional parameters
+$(document).ready(function (){
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 1
+  });
+});
 
-  loop: false,
 
-  // If we need pagination
-  pagination: {
-    el: ".chocolate-swiper-pagination",
-    clickable: true,
-    dynamicBullets: false,
-  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const swiper = new Swiper(".chocolate-slider", {
+//   // Optional parameters
+
+//   loop: false,
+
+//   // If we need pagination
+//   pagination: {
+//     el: ".chocolate-swiper-pagination",
+//     clickable: true,
+//     dynamicBullets: false,
+//   },
 
   
-  slidesPerView: 1,
-  spaceBetween: 30,
-    // autoplay: {
-    //   delay: 5000,
-    //   disableOnInteraction: false,
-    //   stopOnLastSlide: false,
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//     // autoplay: {
+//     //   delay: 5000,
+//     //   disableOnInteraction: false,
+//     //   stopOnLastSlide: false,
 
-    // },
-  speed: 1500,
-    breakpoints: {
-      // when window width is >= 320px
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      },
-      // when window width is >= 480px
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 30
-      },
+//     // },
+//   speed: 1500,
+//     breakpoints: {
+//       // when window width is >= 320px
+//       768: {
+//         slidesPerView: 2,
+//         spaceBetween: 20
+//       },
+//       // when window width is >= 480px
+//       1200: {
+//         slidesPerView: 3,
+//         spaceBetween: 30
+//       },
      
-    }
-});
+//     }
+// });
